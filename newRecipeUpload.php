5e37c7aeed4412647a008d6896575f335ea4a1be
Matt Searle <matthewsearle01@gmail.com>
2021-09-29 14:31:19 +0100

    removed redundant code from newRecipeUpload.php

--- newRecipeUpload.php
+++ newRecipeUpload.php
@@ -3,12 +3,6 @@
 require_once ('includes/dbconnection.php');
 require_once ('includes/functions.php');
 
-$valid = true;
-
-if (!$valid) {
-    header('Location: addEntry.php');
-}
-
 $recipeAdded = addNewRecipe($db, $_POST);
 
 if ($recipeAdded) {
